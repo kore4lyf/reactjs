c972fe4f0d39a4acc1a8a96c1d61c59d114cce05
Korede Faleye <akfaleye@gmail.com>
2023-09-01 11:36:02 +0100

    Make moveto container hide onMouseOut

--- myreads/src/Book-bup.js
+++ myreads/src/Book-bup.js
@@ -1,20 +1,15 @@
-import { Component } from 'react'
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
   
-class Book extends Component {
-  constructor(props) {
-    super(props)
-  }
-  
-  getNumOfAuthors = book => {
+function Book({book, functions}) {
+  const getNumOfAuthors = book => {
     if(book.authors) return book.authors.length;
   }
 
-  getAuthors = book => { 
+  const getAuthors = book => { 
     if (book.authors) return book.authors;
   }
 
-  getImageLink = book => {
+  const getImageLink = book => {
     if(book.imageLinks) {
       if(book.imageLinks.thumbnail) {
         return book.imageLinks.thumbnail;
@@ -24,24 +19,19 @@ class Book extends Component {
     }
   }
 
-
-  render() {
-  
-  const { book, functions } = this.props
   return(
-    <div className="book" 
-        onMouseLeave={() => this.movetoContainer.classList.add('hide')}>
+    <div className="book">
 
       <div className="image-container">
         {book.imageLinks !== undefined ?
-          <img src={this.getImageLink(book)} 
+          <img src={getImageLink(book)} 
           alt={"Cover page for " + book.title}/> :
-            <div className="missing-image"> <p>No Image </p> </div> }
+          <div className="missing-image"> <p>No Image </p> </div> }
          <div className="edit-container">
-          <button onClick={() => this.movetoContainer.classList.remove('hide') } className="edit"> <span className="fas fa-edit"> </span> </button>
+          <button onClick={() => ""} className="edit"> <span className="fas fa-edit"> </span> </button>
          </div>
 
-        <div ref={elem => this.movetoContainer = elem} className="hide moveto-container"> 
+        <div onClick={(e) => e.target.parentNode.classList.add('hide') } className="hide moveto-container"> 
           <ul className="no-list-style move-to">
             <li onClick={() => functions.moveTo(book, "")} className={book.shelf === undefined ? "option active" : "option"}>None</li>
             <li onClick={() => functions.moveTo(book, "currentlyReading")} className={book.shelf === "currentlyReading" ? "option active" : "option"}>Reading</li>
@@ -54,14 +44,13 @@ class Book extends Component {
       <div className="details">
         <p className="title"> {book.title}</p>
         <ul className="author-list no-list-style">
-           {this.getNumOfAuthors(book) > 0 && 
-            this.getAuthors(book).map( (author) => (
+           {getNumOfAuthors(book) > 0 && 
+            getAuthors(book).map( (author) => (
             <li key={author} className="author"> {author} </li>))} 
         </ul>
       </div>
     </div> 
   );
-  }
 }
 
 
