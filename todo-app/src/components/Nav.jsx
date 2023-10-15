f6ef45bc35bcc2842673c2cb3f72363a54fd9c1a
Korede Faleye <akfaleye@gmail.com>
2023-10-15 15:41:49 +0100

    style Nav.jsx

--- todo-app/src/components/Nav.jsx
+++ todo-app/src/components/Nav.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import  { FontAwesomeIcon as FA } from '@fortawesome/react-fontawesome' 
+import { faListAlt, faCalendarCheck } from '@fortawesome/free-regular-svg-icons' 
+
 
 const Nav = () => {
   return (
     <nav>
       <ul className="tab">
-        <li><a className="active">Todo</a></li>
-        <li><a>Goals</a></li>
+        <li>
+          <a className="active nav__title todo" href="#"> <FA icon={faListAlt}/>  
+            <span class="nav__text">
+              <span>Todo</span> 
+            </span>
+          </a>
+        </li>
+        <li>
+          <a className="nav__title goal" href="#"> <FA icon={faCalendarCheck} /> 
+            <span class="nav__text">
+              <span>Goal</span>
+            </span>
+          </a>
+        </li>
       </ul>
     </nav>
   )
--- todo-app/src/components/TodoForm.jsx
+++ todo-app/src/components/TodoForm.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types'
 const TodoForm = () => {
   return (
     <section>
-      <h2> TODO </h2> 
       <form onSubmit={""}>
         <input type="text" name="todo" placeholder="Todo" />
         <input type="submit" value="Submit" />
