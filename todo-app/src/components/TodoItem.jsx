d5b2130751991bd919273fd3ba657b62a9344964
Korede Faleye <akfaleye@gmail.com>
2023-10-20 21:52:23 +0100

    Toggle expand/fold todo text on click

--- todo-app/src/components/TodoItem.jsx
+++ todo-app/src/components/TodoItem.jsx
@@ -12,6 +12,12 @@ import {
 const TodoItem = ({children}) => {
   const [completed, setCompleted] = useState(false)
   const todo = useRef()
+  const todoText = useRef()
+
+  const showHiddenText = () => {
+    if(todoText.current.classList.contains('show-hidden-text')) todoText.current.classList.remove('show-hidden-text')
+    else todoText.current.classList.add('show-hidden-text')
+  }
 
   const handleRemoveTodo = () => {
     todo.current.classList.add('slide-out')
@@ -26,16 +32,16 @@ const TodoItem = ({children}) => {
   }
 
   return (
-    <div ref={todo} className={`todo__item ${completed === true ? 'completed-todo' : ''}`}>
+    <div ref={todo} onClick={showHiddenText} className={`todo__item ${completed === true ? 'completed-todo' : ''}`}>
       <div className={`flex-flow-1rem `}> 
         <div className={`todo__checkbox__container ${completed && 'fade-out'}`}>
           { 
             completed === true ? 
-              <CheckSquare onClick={handleCompleted} size={24}/> : 
-              <Square onClick={handleCompleted} size={24} />
+              <CheckSquare onClick={handleCompleted} size={20}/> : 
+              <Square onClick={handleCompleted} size={20} />
           }
         </div>
-        <p className={`todo__text ${completed && 'strike fade-out'}`}>{children}</p>
+        <p ref={todoText} className={`todo__text ${completed && 'strike fade-out'}`}>{children}</p>
       </div>
       <div className="todo__close__container">
         <X className="todo__close" onClick={handleRemoveTodo} />
