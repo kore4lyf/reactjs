654fdd0963143c89a394222ac7e59930762a1dee
Korede Faleye <akfaleye@gmail.com>
2023-10-24 13:31:05 +0100

    Adjust navbar bottom value

--- todo-app/src/components/GoalItem.jsx
+++ todo-app/src/components/GoalItem.jsx
@@ -30,13 +30,14 @@ const GoalItem = ({goal,todos}) => {
           <MoreVertical />
         </button>
       </div>
-
-      <ul className="goal__todos">
-        {todos.map( (todo) => (
-          <li key={todo.id}> 
-            <TodoItem  text={todo.text} /> 
-          </li>))}
-      </ul>
+      <div className="goal__todo__container"> 
+        <ul className="goal__todos">
+          {todos.map( (todo) => (
+            <li key={todo.id}> 
+             <TodoItem  text={todo.text} /> 
+            </li>))}
+        </ul>
+      </div>
     </div>
   )
 }
