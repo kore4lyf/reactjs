7a9c3ba61057cf8e148d92300629710b3b1c9186
Korede Faleye <akfaleye@gmail.com>
2023-11-06 11:07:00 +0100

    Route title to homepage

--- myreads/src/Header.jsx
+++ myreads/src/Header.jsx
@@ -1,9 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types';
 
 function Header({children}) {
   return(
       <header>
-        <h1 className="header mx-auto w-90"> {children} </h1>
+        <h1 className="header mx-auto w-90">
+          <Link to="/"> 
+            {children} 
+          </Link>
+        </h1>
       </header> 
   );
 }
