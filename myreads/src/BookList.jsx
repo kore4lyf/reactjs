79b9c71dd081611375d085222f62505708831985
Korede Faleye <akfaleye@gmail.com>
2023-11-06 17:17:59 +0100

    Add pre-data fetch skeleton

--- myreads/src/BookList.jsx
+++ myreads/src/BookList.jsx
@@ -1,15 +1,44 @@
 import Book from './Book';
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 import PropTypes from 'prop-types'
 
 function BookList({books, functions}) {
-  return(
-    <div className="book-list">
-      {
-        books.map( (book) => book !== undefined && <Book key={book.id} book={book} functions={functions}/>) 
-      
-      }
-    </div>
-  );
+  console.log('Book Length: ' + books.length)
+    if(books.length === 0) return (
+      <div className="skeleton-container">
+        <div className="skeleton">
+          <Skeleton className="skeleton-img" />
+          <Skeleton className="skeleton-title" />
+          <Skeleton className="skeleton-text"/>
+        </div>
+        
+        <div className="skeleton">
+          <Skeleton className="skeleton-img" />
+          <Skeleton className="skeleton-title" />
+          <Skeleton className="skeleton-text"/>
+        </div>
+        
+        <div className="skeleton">
+          <Skeleton className="skeleton-img" />
+          <Skeleton className="skeleton-title" />
+          <Skeleton className="skeleton-text"/>
+        </div>
+        
+        <div className="skeleton">
+          <Skeleton className="skeleton-img" />
+          <Skeleton className="skeleton-title" />
+          <Skeleton className="skeleton-text"/>
+        </div>
+      </div>
+    )
+    else return (
+      <div className="book-list">
+        {
+          books.map( (book) => book !== undefined && <Book key={book.id} book={book} functions={functions}/>) 
+        }
+      </div>
+    )
 }
 
 
